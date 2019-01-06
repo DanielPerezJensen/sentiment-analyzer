e3fbed933359be1f9e901177d37b3774b5f37e06
Daniel Perez Jensen <danielperezjensen@gmail.com>
2019-01-06 21:44:40 +0100

    Renamed main.py to example.py

--- example.py
+++ example.py
@@ -2,4 +2,4 @@ from sentiment import sentiment
 
 
 print(sentiment("This is great, AAPL stock will surely go up!"))
-print(sentiment("Not looking good tomorrow, predicting that Apple will be Bearish"))
+print(sentiment("Not looking good tomorrow, Apple will go down"))
